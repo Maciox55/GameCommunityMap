739778c4f72a51d69ec767a321eb7651141ed5a4
Maciej Bregisz <maciox55@gmail.com>
2018-09-14 19:51:43 -0400

    test

--- src/server/server.js
+++ src/server/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '../dist/SCUMMAP'));
+app.use(express.static(__dirname + '../dist/scummap'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'../dist/SCUMMAP/index.html'));
+res.sendFile(path.join(__dirname+'../dist/scummap/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
