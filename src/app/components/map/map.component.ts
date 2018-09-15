6a06a342071a7fa73b73ca8e70e9985f110abd1d
Maciej Bregisz <maciox55@gmail.com>
2018-09-15 16:04:12 -0400

    Tiles Fixed?

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,12 +15,14 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var imageBounds = [[66, -90], [-66, 90]];
+    var imageBounds = [[-90, 180], [90, -180]];
     var trechville = [[15.989602069938704,55.733642578125],[21.989602069938704,61.733642578125]];
 
     var map = L.map('map').setView(defaultCoords,defaultZoom);
-    L.tileLayer('/dist/SCUMMap/assets/SCUMTILES/{z}/{x}/{y}.png',{attribution:"Maciox55",minZoom: 1,maxZoom: 7,maxNativeZoom:3}).addTo(map);
-    L.imageOverlay('/dist/SCUMMap/assets/Trenchylvania.png', trechville,{opacity:0.9}).addTo(map);
+    //L.imageOverlay('assets/SCUM_Map.png',imageBounds,{attribution:"Maciox55"}).addTo(map);
+    
+    L.tileLayer('assets/SCUMTILES/{z}/{x}/{y}.png',{attribution:"Maciox55",minZoom: 1,maxZoom: 7,maxNativeZoom:3,noWrap:true}).addTo(map);
+    L.imageOverlay('assets/Trenchylvania.png', trechville,{opacity:0.9}).addTo(map);
 
     map.on('click', function(e) {
       alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
--- src/server/server.js
+++ src/server/server.js
@@ -5,18 +5,30 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/../../dist/SCUMMap/'));
+const allowedExt = [
+    '.js',
+    '.ico',
+    '.css',
+    '.png',
+    '.jpg',
+    '.woff2',
+    '.woff',
+    '.ttf',
+    '.svg',
+  ];
+  app.use('/dist/', express.static(__dirname+'/dist/SCUMMap/'))
 
-app.use('/',express.static(__dirname + '/../../dist/SCUMMap/assets/SCUM_Map.png'));
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/../../dist/SCUMMap/index.html'));
-console.log(__dirname+'/../../dist/SCUMMap/index.html');
+app.get('*', function(req,res) {
+    if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+        res.sendFile(path.resolve(`dist/SCUMMap/${req.url}`));
+      } else {
+        res.sendFile(path.resolve('dist/SCUMMap/index.html'));
+      }
 });
 
 
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
