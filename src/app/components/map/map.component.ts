c67745c7938860e1c39bcc17596fe2cf6a883b36
Maciej Bregisz <maciox55@gmail.com>
2018-09-15 21:09:32 -0400

    Added more locations, Beginning to add bounds and labels

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,18 +15,43 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var imageBounds = [[-90, 180], [90, -180]];
-    var trechville = [[15.989602069938704,55.733642578125],[21.989602069938704,61.733642578125]];
+    var map = L.map('map',{crs:L.CRS.Simple}).setView(defaultCoords,defaultZoom);
+    var towns = {trenchville:[[-116.46035048129474,167.53125],[-111.41907350763373, 172.15625]],
+                 dewhurst: [[-235.73978570924862, 133.84375],[-228.1172054053101, 141.84375]],
+                 craydon:[[-28.684182834702582, 66.71875],[-19.062450416064316, 76.59375]],
+                 oakheart:[[],[]],
+                 blackisle:[[],[]]
+    }
 
-    var map = L.map('map').setView(defaultCoords,defaultZoom);
+    var townLabels = [{pos:[-116.46035048129474,167.53125],
+      labelText:'Trenchville'}];
+      
+    
+    var markers = [];
+    
+
+    var imageBounds = [[-90, 180], [90, -180]];
     //L.imageOverlay('assets/SCUM_Map.png',imageBounds,{attribution:"Maciox55"}).addTo(map);
     
-    L.tileLayer('assets/SCUMTILES/{z}/{x}/{y}.png',{attribution:"Maciox55",minZoom: 1,maxZoom: 7,maxNativeZoom:3,noWrap:true}).addTo(map);
-    L.imageOverlay('assets/Trenchylvania.png', trechville,{opacity:0.9}).addTo(map);
+    L.circle([-116.46035048129474,167.53125], {radius: 10}).addTo(map).on("click", circleClick);
+    L.tileLayer('assets/SCUMTILES/{z}/{x}/{y}.png',{attribution:"Maciox55",minZoom: 1,maxZoom: 8,maxNativeZoom:3,noWrap:true}).addTo(map);
+    L.imageOverlay('assets/Trenchylvania.png', towns.trenchville,{opacity:1}).addTo(map);
+    L.imageOverlay('assets/DewhurstCorrected.png', towns.dewhurst,{opacity:1}).addTo(map);
+    L.imageOverlay('assets/Craydon.png', towns.craydon,{opacity:0.9}).addTo(map);
 
+    for(var i=0; i<townLabels.length;i++)
+    {
+        L.marker(townLabels[i].pos,{title:townLabels[i].labelText, zIndexOffset:100}).addTo(map);
+    }
     map.on('click', function(e) {
-      alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+      console.log(e);
     });
+    function circleClick(e) {
+      var clickedCircle = e.target;
+  
+    // do something, like:
+    clickedCircle.bindPopup("some content").openPopup();
+  }
 
   }
 
