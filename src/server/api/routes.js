e3dbc3dd1881d0f1e7f9dc7329f6aa4389276599
Maciej Bregisz <maciox55@gmail.com>
2018-09-18 03:38:48 -0400

    Fixed missing config

--- src/server/api/routes.js
+++ src/server/api/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const ObjectID = require('mongodb').ObjectID;
-const config = require('../config');
+const config = require('../');
 const User = require('../models/User');
 
 
---
+++ src/server/config.js
@@ -0,0 +1,3 @@
+module.export={
+    dbCreds:"Test61234"
+}
